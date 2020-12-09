9eab539929c4d150f1ecd0a3e3a9cb4b5a615744
Jeremy-github <55029664+Jeremy-github@users.noreply.github.com>
2020-12-09 18:44:45 -0500

    Add files via upload

---
+++ database/db.sql
@@ -0,0 +1,39 @@
+
+
+CREATE DATABASE DB_RAJ_STORE; 
+
+USE DB_RAJ_STORE;
+
+
+CREATE TABLE usuarios(
+    id INT(11) NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    nombre VARCHAR(60) NOT NULL,
+    apellido VARCHAR(60) NOT NULL,
+    correo VARCHAR(100) NOT NULL
+);
+ALTER TABLE usuarios 
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE usuarios 
+    MODIFY id INT(11) 
+    NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+INSERT INTO USUARIOS(id, username, password, nombre, apellido, correo, celular) 
+
+    DESCRIBE usuarios;
+
+CREATE TABLE productos(
+    id INT(11) NOT NULL,
+    nombre VARCHAR(60) NOT NULL,
+    img  TEXT NOT NULL,
+    categoria VARCHAR (40) NOT NULL ,
+    descripcion VARCHAR (200) NOT NULL
+);
+ALTER TABLE productos 
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE productos
+    MODIFY id INT(11) 
+    NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
